4ad00914410e4fda632c36c7621c237ae5179adb
Aldila Iskandar <aldila.iskandar@dkatalis.com>
2024-05-10 18:06:59 +0700

    feat: initial commit

---
+++ src/main/java/com/aldila/kiilogram/KiilogramApplication.java
@@ -0,0 +1,13 @@
+package com.aldila.kiilogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiilogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiilogramApplication.class, args);
+	}
+
+}
